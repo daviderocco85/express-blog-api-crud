83bc6e0ba61c00879b49e834a79a3073f18cc0c6
daviderocco85 <roccodavide85@gmail.com>
2026-09-07 18:26:32 +0200

    feat: update Create, Update and Modify logic in postController.js

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -29,16 +29,70 @@ export const getById = (req, res) => {
 };
 
 export const create = (req, res) => {
-    console.log(req.body);
-    res.send(`Creazione dell'articolo del blog`);
+    const newId = postsList.at(-1).id + 1;
+
+    const newPost = {
+        id: newId,
+        title: req.body.title,
+        content: req.body.content,
+        image: req.body.image,
+        tags: req.body.tags
+    };
+
+    postsList.push(newPost);
+
+    res.status(201).json(newPost);
 };
+
 
 export const update = (req, res) => {
-    res.send(`Aggiornamento totale dell'articolo del blog con id: ${req.params.id}`);
-};
+
+    const id = Number(req.params.id);
+
+    const post = postsList.find(p => p.id === id);
+
+    if (!post) {
+        res.status(404).json({
+            error: `Articolo con id ${id} non trovato`
+        });
+        return;
+    }
+
+    if (!req.body.title || !req.body.content || !req.body.image || !req.body.tags) {
+        res.status(400).json({
+            error: "L'aggiornamento totale richiede tutte le proprietà: title, content, image, tags"
+        });
+        return;
+    }
+
+    post.title = req.body.title;
+    post.content = req.body.content;
+    post.image = req.body.image;
+    post.tags = req.body.tags;
+
+    res.json(post);
+
+}
+
 
 export const modify = (req, res) => {
-    res.send(`Aggiornamento parziale dell'articolo del blog con id: ${req.params.id}`);
+    const id = Number(req.params.id);
+
+    const post = postsList.find(p => p.id === id);
+
+    if (!post) {
+        res.status(404).json({
+            error: `Articolo con id ${id} non trovato`
+        });
+        return;
+    }
+
+    if (req.body.title !== undefined) post.title = req.body.title;
+    if (req.body.content !== undefined) post.content = req.body.content;
+    if (req.body.image !== undefined) post.image = req.body.image;
+    if (req.body.tags !== undefined) post.tags = req.body.tags;
+
+    res.json(post);
 };
 
 export const deleteById = (req, res) => {
