46dffa7836ec6b3da83e91835e14b27b7f552c6f
daviderocco85 <roccodavide85@gmail.com>
2026-09-08 11:26:58 +0200

    feat: enable static assets serving from public directory

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ import { postsRouter } from './routers/postsRouter.js';
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 app.use(express.json());
+
 app.use('/posts', postsRouter);
 
 app.listen(port, () => {
