43b31541bf82de402360d8f43af5177a8660aaed
daviderocco85 <roccodavide85@gmail.com>
2026-09-09 15:04:33 +0200

    chore: register notFound and errorsHandler middlewares

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { postsRouter } from './routers/postsRouter.js';
+import { notFound } from './middlewares/notFound.js';
+import { errorsHandler } from './middlewares/errorsHandler.js';
 const app = express();
 const port = 3000;
 
@@ -8,6 +10,10 @@ app.use(express.static('public'));
 app.use(express.json());
 
 app.use('/posts', postsRouter);
+
+app.use(notFound);
+
+app.use(errorsHandler);
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
